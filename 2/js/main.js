1ffc8b92b975c209bdf0f06194247d67c493e745
Keks <keks@htmlacademy.ru>
2022-09-26 08:28:32 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,7 +1,7 @@
-const getRandomInteger = (from, to) => {  
+const getRandomInteger = (from, to) => {
   if(from < 0 || to < 0) {
-    return 0
-    }
+    return 0;
+  }
   return Math.floor(Math.random() * (to - from)) + from; // взято с https://myrusakov.ru/js-random-numbers.html
 };
 getRandomInteger(4,10);
@@ -9,6 +9,6 @@ getRandomInteger(4,10);
 
 const checkLenght = (stringToCheck, maxLenght) => {
   stringToCheck = String(stringToCheck); //ввёл, чтобы не было проблем с типами данных
- return stringToCheck.length <= maxLenght ? True : False;
+  return stringToCheck.length <= maxLenght ? True : False;
 };
 checkLenght('sfff', 8);
