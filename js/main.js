4fa99646c2ba9b396bdd661b4af29d90afeef6ea
Artyom <113242488+Akulinych@users.noreply.github.com>
2022-10-02 20:33:05 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-const getRandomInteger = (from, to) => {
-  if(from < 0 || to < 0) {
-    return 0;
-  }
-  return Math.floor(Math.random() * (to - from)) + from; // взято с https://myrusakov.ru/js-random-numbers.html
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
-getRandomInteger(4,10);
+getRandomPositiveInteger(4,10);
 
 
-const checkLenght = (stringToCheck, maxLenght) => {
-  stringToCheck = String(stringToCheck); //ввёл, чтобы не было проблем с типами данных
-  return stringToCheck.length <= maxLenght ? 1 : 0;
+const checkStringLength = (string, length) => {
+   return string.length <= length;
 };
 checkLenght('sfff', 8);
