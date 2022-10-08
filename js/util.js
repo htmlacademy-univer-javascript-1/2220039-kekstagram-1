97b39aca41f7a371156eb036570e9bca5cb46ef3
Artyom <113242488+Akulinych@users.noreply.github.com>
2022-10-08 23:17:27 +0500

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,1 +1,8 @@
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+};
+const checkStringLength = (string, length) => string.length <= length;
 
+export {getRandomPositiveInteger, checkStringLength};
