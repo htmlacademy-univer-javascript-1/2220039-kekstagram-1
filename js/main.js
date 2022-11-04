6afebb706791b117e1fb81210a8179567eea6293
Artyom <113242488+Akulinych@users.noreply.github.com>
2022-11-04 21:11:25 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-import { getRandomPositiveInteger, checkStringLength } from './util.js';
 import { createPhotosArray } from './data.js';
 
-getRandomPositiveInteger(10,2);
-checkStringLength('sd', 4);
 createPhotosArray();
