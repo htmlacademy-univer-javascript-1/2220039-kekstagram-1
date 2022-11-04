b39007f41a2508fc19b7389e014b5f4c021f117f
Artyom <113242488+Akulinych@users.noreply.github.com>
2022-11-04 21:09:07 +0500

    Create consts.js

---
+++ js/consts.js
@@ -0,0 +1,11 @@
+const CountLike = {
+  MIN: 15,
+  MAX: 200
+};
+const AvatarNumber = {
+  MIN: 1,
+  MAX: 6
+};
+const MAX_COUNT_PHOTOS = 25;
+
+export { CountLike, AvatarNumber, MAX_COUNT_PHOTOS  };
