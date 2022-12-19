101938d6cb5f61d6ef47c63804e61602a697dff1
Akulinych <SemenovAA@urfu.me>
2022-12-19 23:30:42 +0500

    module12-task1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { renderPhotos } from './pictures.js';
 import { sendRequest } from './fetch.js';
+import { onFail } from './utils.js';
 import './big-picture.js';
 import './form.js';
 import './form-validation.js';
@@ -11,20 +12,6 @@ let photos = [];
 const onSuccess = (data) => {
   photos = data.slice();
   renderPhotos(photos);
-};
-
-const onFail = () => {
-  const messageAlert = document.createElement('div');
-  messageAlert.style.position = 'absolute';
-  messageAlert.style.left = 0;
-  messageAlert.style.top = 0;
-  messageAlert.style.right = 0;
-  messageAlert.style.textAlign = 'center';
-  messageAlert.style.fontSize = '30px';
-  messageAlert.style.backgroundColor = 'red';
-  messageAlert.style.padding = '10px 5px';
-  messageAlert.textContent = 'Ошибка загрузки данных';
-  document.body.append(messageAlert);
 };
 
 sendRequest(onSuccess, onFail, 'GET');
