cffdd067e8f9047c254a73983c6985e75f4ff5a8
Akulinych <SemenovAA@urfu.me>
2022-12-19 22:59:23 +0500

    module11-task1

--- js/utils.js
+++ js/utils.js
@@ -15,4 +15,18 @@ const numberDeclination = (num, nominative, genitiveSingular, genitivePlural) =>
   return genitivePlural;
 };
 
-export { isEscKey, numberDeclination };
+const onFail = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.textAlign = 'center';
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.padding = '10px 5px';
+  messageAlert.textContent = 'Ошибка загрузки данных';
+  document.body.append(messageAlert);
+};
+
+export { isEscKey, numberDeclination, onFail };
